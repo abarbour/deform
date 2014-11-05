52dad07f1e1bd8ba8162450ee468c5dbd35a3045
Andrew Barbour <andy.barbour@gmail.com>
2014-11-05 00:19:06 -0800

    Working on Segall stuff

--- NOBUILD/Sandbox/segall.R
+++ NOBUILD/Sandbox/segall.R
@@ -1,10 +1,15 @@
 library(deform)
+library(TeachingDemos)
 
-y. <- c(-10:-2, seq(-1.9,1.9,by=0.1), 2:10)
-su <- surface_displacement(y.*1e3, C.=1e13, z_src=1e3)
-#plot(uz ~ y, su, type="b", ylim=c(-2,1)*10)
-#lines(uy ~ y, su, type="b", col="red")
-#lines((uy+uz) ~ y, su, type="b", col="blue")
+x. <- c(-10:-2, seq(-1.9,1.9,by=0.1), 2:10)
+su <- surface_displacement(x.*1e3, C.=1e13, z_src=0.7e3)
+
+plot(uz ~ x, su, type="b", ylim=c(-1,1)*15); text(4e3, -3, "U_z")
+lines(ux ~ x, su, type="b", col="red"); text(-1e3, 3, "U_x", col="red")
+lines(uxz.mag ~ x, su, type="b", col="blue", pch=16, cex=0.6); text(-3e3, 8, "|U_xz|", col="blue")
+my.symbols(x=su$x, y=su$uxz.mag, ms.arrows, angle=su$uxz.ang, adj=0, col="blue", inches=0.4, add=TRUE)
+
+plot(c(NA,diff(ux)/diff(x),NA) ~ c(NA,x), su, type="s"); abline(v=0,h=0,col="grey")
 
 # Fig 8
 #y. <- 0:10
@@ -14,6 +19,5 @@ D. <- 1e3
 L. <- 10e3
 B. <- 0.6
 c. <- 0.1
-matplot(y.,t.,
-  timevarying_surface_displacement(y., t., Vdot, B., L., D., c., y_src.=1),
-  type="l")
+zz2 <- timevarying_surface_displacement(x., t., Vdot, B., L., D., c., x_src.=1)
+#matplot(x., zz2, type="l")
--- R/segall85.R
+++ R/segall85.R
@@ -3,17 +3,17 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' y. <- c(-10:-2, seq(-1.9,1.9,by=0.1), 2:10)
-#' su <- surface_displacement(y.*1e3, z_src=1e3)
-#' plot(uz ~ y, su, type="b")
+#' x. <- c(-10:-2, seq(-1.9,1.9,by=0.1), 2:10)
+#' su <- surface_displacement(x.*1e3, z_src=1e3)
+#' plot(uz ~ x, su, type="b")
 #' }
 segall85 <- function(){
 }
 
 #  spatial coordinates in segall
-#   x is positive downwards
-#   y is positive away from source
-#   z is along line source
+#   x is positive downwards (here z)
+#   y is positive away from source (here x)
+#   z is along line source (here y)
 #  source coordinates:
 #   z_src is zeta (depth)
 #   y_src is xi (distance away)
@@ -22,43 +22,46 @@ segall85 <- function(){
 #   C units of force, proportional to the source strength
 #' @rdname segall85
 #' @export
-surface_displacement <- function(y, C.=1, mu.=1e9, ...){
-  sg <- .surface_g(y, ...)
+surface_displacement <- function(x, C.=1, mu.=1e9, ...){
+  sg <- .surface_g(x, ...)
   c. <- C./mu.
-  sg$uz <- sg$gz * c.
-  sg$uy <- sg$gy * c.
+  sg <- mutate(sg,
+               ux = gx * c.,
+               uz = gz * c.,
+               uxz.mag = sqrt(ux^2 + uz^2),
+               uxz.ang = atan2(uz,ux) * 180/pi )
   return(sg)
 }
 
 #' @rdname segall85
 #' @export
-.surface_g <- function(y=0, z_src=0, y_src=0, nuu=1/3){
+.surface_g <- function(x=0, x_src=0, z_src=0, nuu=1/3){
   # segall85 C9
-  gz <- -2*(1 - nuu)*z_src/(z_src^2 + (y - y_src)^2)
-  gy <-  2*(1 - nuu)*(y - y_src)/(z_src^2 + (y - y_src)^2)
-  data.frame(y, gz, gy)
+  gx <-  2*(1 - nuu)*(x - x_src)/(z_src^2 + (x - x_src)^2)
+  gz <- -2*(1 - nuu)*z_src/(z_src^2 + (x - x_src)^2)
+  data.frame(x, gx, gz)
 }
 
 # Time varying deformation associatedw with fluid extraction
 
 #' @rdname segall85
 #' @export
-timevarying_surface_displacement <- function(y, Time, Vdot., B., L., D., HD., nuu.=1/3, y_src.=0){
+timevarying_surface_displacement <- function(x, Time, Vdot., B., L., D., HD., nuu.=1/3, x_src.=0){
   # segall85 eq 26
   # at each time slice, calculate a profile
-  .tvsd <- function(yi, ti, .Vdot, .B, .L, .D, .HD, .nuu, .y_src){
-    message(paste(.Vdot, .B, .L, .D, .HD, .nuu, .y_src))
+  .tvsd <- function(xi, ti, .Vdot, .B, .L, .D, .HD, .nuu, .x_src){
+    message(paste(.Vdot, .B, .L, .D, .HD, .nuu, .x_src))
     # source
     mod <- 2*.B*(1 + .nuu)*.Vdot*.D*sqrt(ti/.HD)/(3*pi*.L)
     # line source correction
-    sc <- if (.y_src==0){
+    sc <- if (.x_src==0){
       1
     } else {
-      Time.src <- sqrt(.y_src^2 / 4 / .HD / ti)
+      Time.src <- sqrt(.x_src^2 / 4 / .HD / ti)
       err <- qnorm(Time.src/2, lower = FALSE)/sqrt(2)  #ierfc
-      sum(err/(.D^2 + (yi - .y_src)^2), na.rm=TRUE)
+      sum(err/(.D^2 + (xi - .x_src)^2), na.rm=TRUE)
     }
     return(mod*sc)
   }
-  outer(X=y, Y=Time, FUN=.tvsd, .Vdot=Vdot., .B=B., .L=L., .D=D., .HD=HD., .nuu=nuu., .y_src=y_src.)
+  outer(X=x, Y=Time, FUN=.tvsd, .Vdot=Vdot., .B=B., .L=L., .D=D., .HD=HD., .nuu=nuu., .x_src=x_src.)
 }
